6359deb197542952f9e1bf4b6a69932579869c1d
MaksymMazur <mm@intravision.dk>
2025-02-22 15:10:21 +0200

    Improved views

--- src/main/java/org/statistic/eggs/Controller.java
+++ src/main/java/org/statistic/eggs/Controller.java
@@ -31,6 +31,10 @@ import java.util.List;
 import java.util.Map;
 
 public class Controller {
+
+    // TODO: Implement manual add
+    @FXML
+    public TextField addManually;
     @FXML
     private Label prevResults;
 
